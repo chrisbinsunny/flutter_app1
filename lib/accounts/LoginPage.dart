6c5a21aa5280141f8131a5cf227cc7af937f9403
Chrisbin Sunny <chrisbinsunny1@gmail.com>
2022-02-10 01:14:15 +0530

    Added accounts

---
+++ lib/accounts/LoginPage.dart
@@ -0,0 +1,286 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import '../HomeBase.dart';
+import '../Resources/AppColors.dart';
+import '../accounts/RegisterPage.dart';
+import '../backend/backend_data.dart';
+import '../sizes.dart';
+import '../widgets.dart';
+
+class LoginPage extends StatefulWidget {
+  @override
+  _LoginPageState createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  TextEditingController emailId = TextEditingController();
+  TextEditingController password = TextEditingController();
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SingleChildScrollView(
+        child: Container(
+          width: screenWidth(context),
+          height: screenHeight(context),
+          padding: EdgeInsets.only(
+            top: screenHeight(context, mulBy: 0.03)
+          ),
+          color: Color(AppColors.commonOrange),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              _loginComponents(),
+              _toRegister()
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget _toRegister() {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Text("Don't have an Account?"),
+        TextButton(
+          onPressed: () {
+            Navigator.push(
+              context,
+              MaterialPageRoute(builder: (context) => RegisterPage()),
+            );
+          },
+          child: Text(
+            "Sign Up Here",
+            style:
+                TextStyle(fontWeight: FontWeight.bold, color: Colors.white),
+          ),
+        )
+      ],
+    );
+  }
+
+  Widget _loginComponents() {
+    return Container(
+      padding: EdgeInsets.symmetric(
+        vertical: screenHeight(context, mulBy: 0.03),
+          horizontal: screenWidth(context, mulBy: 0.08)
+      ),
+      width: screenWidth(context, mulBy: 0.9),
+      height: screenHeight(context, mulBy: 0.75),
+      decoration: BoxDecoration(
+        color: Colors.white,
+        borderRadius: BorderRadius.all(
+          Radius.circular(20.0),
+        ),
+        boxShadow: [
+          BoxShadow(
+            color: Colors.grey.withOpacity(0.5),
+            spreadRadius: 8,
+            blurRadius: 7,
+            offset: Offset(0, 3), // changes position of shadow
+          ),
+        ],
+      ),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: [
+          Image.asset('images/app_logo.png'),
+
+          _socialMedia(),
+
+          Column(
+            children: [
+              TextField(
+              controller: emailId,
+                decoration: InputDecoration(
+                  hintText: 'Enter Email Address',
+                  contentPadding: EdgeInsets.symmetric(
+                      vertical: 10.0, horizontal: 20.0),
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.all(Radius.circular(32.0)),
+                  ),
+                  enabledBorder: OutlineInputBorder(
+                    borderSide: BorderSide(
+                        color: Color(AppColors.grey), width: 1.0),
+                    borderRadius: BorderRadius.all(Radius.circular(32.0)),
+                  ),
+                  focusedBorder: OutlineInputBorder(
+                    borderSide: BorderSide(
+                        color: Color(AppColors.commonOrange), width: 2.0),
+                    borderRadius: BorderRadius.all(Radius.circular(32.0)),
+                  ),
+                ),
+              ),
+              SizedBox(height: screenHeight(context, mulBy: 0.015)),
+              TextField(
+                  controller: password,
+                  decoration: InputDecoration(
+                    hintText: 'Enter Password',
+                    contentPadding: EdgeInsets.symmetric(
+                        vertical: 10.0, horizontal: 20.0),
+                    border: OutlineInputBorder(
+                      borderRadius:
+                      BorderRadius.all(Radius.circular(32.0)),
+                    ),
+                    enabledBorder: OutlineInputBorder(
+                      borderSide: BorderSide(
+                          color: Color(AppColors.grey), width: 1.0),
+                      borderRadius:
+                      BorderRadius.all(Radius.circular(32.0)),
+                    ),
+                    focusedBorder: OutlineInputBorder(
+                      borderSide: BorderSide(
+                          color: Color(AppColors.commonOrange),
+                          width: 2.0),
+                      borderRadius:
+                      BorderRadius.all(Radius.circular(32.0)),
+                    ),
+                  ),
+                  keyboardType: TextInputType.visiblePassword,
+                  obscureText: true,
+                  obscuringCharacter: '*'),
+              SizedBox(height: screenHeight(context, mulBy: 0.04)),
+              ConstrainedBox(
+                constraints:
+                BoxConstraints.tightFor(width: screenWidth(context), height: screenHeight(context, mulBy: 0.07)),
+                child: ElevatedButton(
+                  style: ButtonStyle(
+                      shape:
+                      MaterialStateProperty.all<RoundedRectangleBorder>(
+                          RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(50.0),
+                          )),
+                      backgroundColor: MaterialStateProperty.all(
+                          Color(AppColors.commonOrange))),
+                  onPressed: () async{
+                    if(emailId.text.isValidEmail()) {
+                      showLoaderDialog(context);
+                      login(emailId.text, password.text).then((value)
+                      {
+                        if(value==200) {
+                          Navigator.pop(context);
+                          Navigator.of(context).pushAndRemoveUntil(MaterialPageRoute(builder: (context) =>
+                          HomeBase()), (Route<dynamic> route) => false);
+                        }
+                        else {
+                          if(value==401) {
+                            Navigator.pop(context);
+                            showAlertDialog1(context, "Invalid Credentials", "Please check the data you entered. Or Register Now.");
+                          }
+                          else{
+                            Navigator.pop(context);
+                            showAlertDialog1(context, "Error Occurred", "Some error occurred. Please try again.");
+                          }
+                        }
+                      }
+                      );
+
+                    }
+                    else {
+                      ScaffoldMessenger.of(context).showSnackBar(
+                        const SnackBar(
+                          content: Text("Please check your email."),
+                        ),
+                      );
+                    }
+                  },
+                  child: Text("Login"),
+                ),
+              ),
+            ],
+          ),
+          _orBox(),
+        ],
+      ),
+    );
+  }
+
+  Widget _orBox() {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.end,
+      children: [
+        Text(
+          'Forgot Password?',
+          style: TextStyle(
+              fontWeight: FontWeight.bold,
+              color: Color(AppColors.commonOrange)),
+        ),
+      ],
+    );
+  }
+
+  Widget _socialMedia() {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: [
+        Container(
+          width: 60,
+          height: 60,
+          decoration: BoxDecoration(
+            color: Colors.white,
+            shape: BoxShape.circle,
+            boxShadow: [
+              BoxShadow(
+                color: Colors.grey.withOpacity(0.3),
+                spreadRadius: 2,
+                blurRadius: 3,
+                offset: Offset(0, 3), // changes position of shadow
+              ),
+            ],
+          ),
+          padding: const EdgeInsets.all(12.0),
+          child: Image.asset(
+            'images/fb_ico_png.png',
+            fit: BoxFit.cover,
+          ),
+        ),
+
+        Container(
+          width: 60,
+          height: 60,
+          decoration: BoxDecoration(
+            color: Colors.white,
+            shape: BoxShape.circle,
+            boxShadow: [
+              BoxShadow(
+                color: Colors.grey.withOpacity(0.3),
+                spreadRadius: 2,
+                blurRadius: 3,
+                offset: Offset(0, 3), // changes position of shadow
+              ),
+            ],
+          ),
+          padding: const EdgeInsets.all(12.0),
+          child: Image.asset(
+            'images/google_ico.png',
+            fit: BoxFit.cover,
+          ),
+        ),
+
+        Container(
+          width: 60,
+          height: 60,
+          decoration: BoxDecoration(
+            color: Colors.white,
+            shape: BoxShape.circle,
+            boxShadow: [
+              BoxShadow(
+                color: Colors.grey.withOpacity(0.3),
+                spreadRadius: 2,
+                blurRadius: 3,
+                offset: Offset(0, 3), // changes position of shadow
+              ),
+            ],
+          ),
+          padding: const EdgeInsets.all(12.0),
+          child: Image.asset(
+            'images/apple_ico.png',
+            fit: BoxFit.cover,
+          ),
+        ),
+      ],
+    );
+  }
+}
---
+++ lib/accounts/RegisterPage.dart
@@ -0,0 +1,254 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import '../Resources/AppColors.dart';
+import '../accounts/LoginPage.dart';
+
+import '../HomeBase.dart';
+
+class RegisterPage extends StatefulWidget{
+  @override
+  _RegisterPageState createState() => _RegisterPageState();
+}
+
+class _RegisterPageState extends State<RegisterPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      resizeToAvoidBottomInset: false,
+      body: Stack(
+        children: [
+          Container(
+            child: SingleChildScrollView(
+              child: Container(
+                width: MediaQuery.of(context).size.width,
+                height: MediaQuery.of(context).size.height,
+                color: Color(AppColors.commonOrange),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Center(child: _registerComponents()),
+                    Center(child: _toLogin())
+                  ],
+                ),
+              ),
+            ),
+          ),
+
+        ],
+      ),
+    );
+  }
+
+  Widget _toLogin(){
+    return Center(
+      child: Padding(
+        padding: const EdgeInsets.only(top: 20.0),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text("Already have an account?"),
+            TextButton(
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => LoginPage()),
+                );
+              },
+              child: Text(
+                "Login Here",
+                style:
+                TextStyle(fontWeight: FontWeight.bold, color: Colors.white),
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+
+
+  Widget _registerComponents() {
+    return Container(
+      width: MediaQuery.of(context).size.width * 0.90,
+      decoration: BoxDecoration(
+        color: Colors.white,
+        borderRadius: BorderRadius.all(
+          Radius.circular(20.0),
+        ),
+        boxShadow: [
+          BoxShadow(
+            color: Colors.grey.withOpacity(0.5),
+            spreadRadius: 8,
+            blurRadius: 7,
+            offset: Offset(0, 3), // changes position of shadow
+          ),
+        ],
+      ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Center(
+                child: Padding(
+                  padding: const EdgeInsets.only(top: 50.0),
+                  child: Image(image: AssetImage('images/app_logo.png')),
+                )),
+            Center(
+              child: Container(
+                height: 420,
+                child: Column(
+                  children: [
+                    Padding(
+                      padding:
+                      const EdgeInsets.only(left: 30.0, right: 30.0, top: 50),
+                      child: TextField(
+                        onChanged: (value) {
+                          //Do something with the user input.
+                        },
+                        decoration: InputDecoration(
+                          hintText: 'Enter Your Name',
+                          contentPadding: EdgeInsets.symmetric(
+                              vertical: 10.0, horizontal: 20.0),
+                          border: OutlineInputBorder(
+                            borderRadius: BorderRadius.all(Radius.circular(32.0)),
+                          ),
+                          enabledBorder: OutlineInputBorder(
+                            borderSide: BorderSide(
+                                color: Color(AppColors.grey), width: 1.0),
+                            borderRadius: BorderRadius.all(Radius.circular(32.0)),
+                          ),
+                          focusedBorder: OutlineInputBorder(
+                            borderSide: BorderSide(
+                                color: Color(AppColors.commonOrange), width: 2.0),
+                            borderRadius: BorderRadius.all(Radius.circular(32.0)),
+                          ),
+                        ),
+                      ),
+                    ),                    Padding(
+                      padding:
+                      const EdgeInsets.only(left: 30.0, right: 30.0, top: 15),
+                      child: TextField(
+                        onChanged: (value) {
+                          //Do something with the user input.
+                        },
+                        decoration: InputDecoration(
+                          hintText: 'Enter Email Address',
+                          contentPadding: EdgeInsets.symmetric(
+                              vertical: 10.0, horizontal: 20.0),
+                          border: OutlineInputBorder(
+                            borderRadius: BorderRadius.all(Radius.circular(32.0)),
+                          ),
+                          enabledBorder: OutlineInputBorder(
+                            borderSide: BorderSide(
+                                color: Color(AppColors.grey), width: 1.0),
+                            borderRadius: BorderRadius.all(Radius.circular(32.0)),
+                          ),
+                          focusedBorder: OutlineInputBorder(
+                            borderSide: BorderSide(
+                                color: Color(AppColors.commonOrange), width: 2.0),
+                            borderRadius: BorderRadius.all(Radius.circular(32.0)),
+                          ),
+                        ),
+                      ),
+                    ),
+                    Padding(
+                      padding:
+                      const EdgeInsets.only(left: 30.0, right: 30.0, top: 15),
+                      child: TextField(
+                          onChanged: (value) {
+                            //Do something with the user input.
+                          },
+                          decoration: InputDecoration(
+                            hintText: 'Enter Password',
+                            contentPadding: EdgeInsets.symmetric(
+                                vertical: 10.0, horizontal: 20.0),
+                            border: OutlineInputBorder(
+                              borderRadius:
+                              BorderRadius.all(Radius.circular(32.0)),
+                            ),
+                            enabledBorder: OutlineInputBorder(
+                              borderSide: BorderSide(
+                                  color: Color(AppColors.grey), width: 1.0),
+                              borderRadius:
+                              BorderRadius.all(Radius.circular(32.0)),
+                            ),
+                            focusedBorder: OutlineInputBorder(
+                              borderSide: BorderSide(
+                                  color: Color(AppColors.commonOrange),
+                                  width: 2.0),
+                              borderRadius:
+                              BorderRadius.all(Radius.circular(32.0)),
+                            ),
+                          ),
+                          keyboardType: TextInputType.visiblePassword,
+                          obscureText: true,
+                          obscuringCharacter: '*'),
+                    ),
+                    Padding(
+                      padding:
+                      const EdgeInsets.only(left: 30.0, right: 30.0, top: 15),
+                      child: TextField(
+                          onChanged: (value) {
+                            //Do something with the user input.
+                          },
+                          decoration: InputDecoration(
+                            hintText: 'Confirm Your Password',
+                            contentPadding: EdgeInsets.symmetric(
+                                vertical: 10.0, horizontal: 20.0),
+                            border: OutlineInputBorder(
+                              borderRadius:
+                              BorderRadius.all(Radius.circular(32.0)),
+                            ),
+                            enabledBorder: OutlineInputBorder(
+                              borderSide: BorderSide(
+                                  color: Color(AppColors.grey), width: 1.0),
+                              borderRadius:
+                              BorderRadius.all(Radius.circular(32.0)),
+                            ),
+                            focusedBorder: OutlineInputBorder(
+                              borderSide: BorderSide(
+                                  color: Color(AppColors.commonOrange),
+                                  width: 2.0),
+                              borderRadius:
+                              BorderRadius.all(Radius.circular(32.0)),
+                            ),
+                          ),
+                          keyboardType: TextInputType.visiblePassword,
+                          obscureText: true,
+                          obscuringCharacter: '*'),
+                    ),
+                    const SizedBox(height: 20),
+                    ConstrainedBox(
+                      constraints:
+                      const BoxConstraints.tightFor(width: 270, height: 45),
+                      child: ElevatedButton(
+                        style: ButtonStyle(
+                            shape:
+                            MaterialStateProperty.all<RoundedRectangleBorder>(
+                                RoundedRectangleBorder(
+                                  borderRadius: BorderRadius.circular(50.0),
+                                )),
+                            backgroundColor: MaterialStateProperty.all(
+                                Color(AppColors.commonOrange))),
+                        onPressed: () {
+
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(builder: (context) => HomeBase()),
+                          );
+
+                        },
+                        child: Text("Sign Up"),
+                      ),
+                    ),
+
+
+
+                  ],
+                ),
+              ),
+            ),
+          ],
+        )
+    );
+  }
+}
