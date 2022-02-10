da7fd1ea7cf4231f41c9765985c3d53e0e4d2fb7
Chrisbin Sunny <chrisbinsunny1@gmail.com>
2022-02-10 05:40:00 +0530

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,44 +1,19 @@
-import 'dart:async';
 import 'package:flutter/material.dart';
-import 'package:flutter_app1/Contact_Us.dart';
-import 'package:flutter_app1/Dashboard.dart';
-import 'package:flutter_app1/Login.dart';
-import 'package:flutter_app1/product_detail.dart';
+import '../HomeBase.dart';
+import '../accounts/LoginPage.dart';
+import '../fragments/HomePage.dart';
+import 'package:flutter/services.dart';
+import '../shared_pref.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
-void main() {
-  runApp(MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(home: MyHomePage());
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  @override
-  Widget build(BuildContext context) {
-    Future.delayed(
-        const Duration(seconds: 3),
-            () => Navigator.push(
-          context,
-          MaterialPageRoute(builder: (context) => Login()),
-        ));
-
-    return Scaffold(
-        body: Center(
-          child: Image(
-            image: AssetImage('images/applogo.png'),
-            width: 200,
-            height: 150,
-          ),
-        )
-    );
-  }
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  SharedPreferences preferences = await SharedPreferences.getInstance();
+  await SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]);
+  bool? userLoggedIn= await HelperFunctions.getUserLoggedIn();
+  runApp(
+      MaterialApp(
+        title: 'PoshRobe',
+        home: (userLoggedIn??false)? HomeBase(): LoginPage(),
+      ));
 }
