9363ca616c2dd076e4713cfff8849ed05654d9b5
Chrisbin Sunny <chrisbinsunny1@gmail.com>
2022-06-18 20:36:19 +0530

    Update AppColors.dart

--- lib/Resources/AppColors.dart
+++ lib/Resources/AppColors.dart
@@ -1,6 +1,8 @@
 
+import 'dart:ui';
+
 class AppColors{
-  static int commonOrange = 0xffFAA51B;
+  static Color commonOrange = Color(0xffFAA51B);
   static int longDividerColor = 0xffF7F7F7;
   static int grey = 0xffBABABA;
   static int homeboxGrey = 0xffF6F6F6;
